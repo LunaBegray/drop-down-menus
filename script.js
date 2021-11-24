6854c163af8cadba22a096c64f6f5f53300ee4ba
luna <lunashoval@gmail.com>
2021-11-24 14:38:17 -0500

    script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+const dropBtn = document.querySelector(".dropMenuBtn");
+const droppedMenuCon = document.querySelector(".droppedMenuCon")
+dropBtn.addEventListener('click', function(){
+    droppedMenuCon.style.display = "content";
+});
